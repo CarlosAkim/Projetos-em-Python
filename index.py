5008b7876833d55e4e0f1e50c03d147ac092f016
Carlos Henrique akim <ch.akim1994@gmail.com>
2023-10-13 10:29:28 -0300

    first commit

---
+++ index.py
@@ -0,0 +1,13 @@
+"""
+print('Calculo para descobrir a area do raio')
+pi = 3.14
+raio = 2 / float(input('Digite o diametro:'))
+area = pi * (raio ** 2)
+print(f'A area do seu circulo é de {area:.2f}')
+"""
+
+real = 1
+dolar = real * 5.05 
+
+produto = dolar * float(input('Quantos dolares custa esse produto?'))
+print(f'Este produto custa {produto} em reais')
