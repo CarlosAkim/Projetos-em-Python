7d4e3b780e29c14450c0fba30266480bb47c2504
Carlos Henrique akim <ch.akim1994@gmail.com>
2023-10-18 13:19:21 -0300

    Update Ordanizador de numeros.py

--- Projetos em Python/Ordanizador de numeros.py
+++ Projetos em Python/Ordanizador de numeros.py
@@ -1,28 +1,28 @@
 numero = [int(input('Digite um valor: '))]
 print('Quantas vezes gostaria de repetir ? :')
 x = int(input(':'))
-k =1
+k = 1
 while k < x:
     numero += [int(input('Digite um valor: '))]
-    k+=1
+    k += 1
 
 
 z = []
 contagem = 0
 for j in numero:
     if z == []:
-       z.append(j)
-    elif j > z[0]:     
+        z.append(j)
+    elif j > z[0]:
         if j > z[contagem-1]:
-                z.insert(contagem,j)
+            z.insert(contagem, j)
         else:
             contador = contagem
             while True:
                 contador = contador - 1
                 if j > z[contador-1]:
-                    z.insert(contador,j)
-                    break                       
+                    z.insert(contador, j)
+                    break
     else:
-        z.insert(0,j)
-    contagem+=1
+        z.insert(0, j)
+    contagem += 1
 print(z)
