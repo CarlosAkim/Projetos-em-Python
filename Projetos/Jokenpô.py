5028abe93cfae62763d4e7678c2ac1dcb45cd8ca
Carlos Henrique dias Akim <140402022+CarlosAkim@users.noreply.github.com>
2024-06-25 17:02:22 +0000

    atualizamos um projeto antigo, com comentarios e novas funcoes que, acredito que esteja mas facil a leitura

--- Projetos/Jokenpô.py
+++ Projetos/Jokenpô.py
@@ -1,88 +1,140 @@
 from random import choice
+
 jogador_vitoria = 0
 maquina_vitoria = 0
 jogador_funcao = 0
 maquina_funcao = 0
 
-def jogador():
-    esc_jogador = input('Escolha entre Pedra, Papel ou Tesoura: ')
+
+def jogador() -> str:
+    """
+    Função para obter a escolha do jogador em um jogo de Pedra, Papel e Tesoura.
+
+    A função solicita ao usuário que escolha entre "pedra", "papel" ou "tesoura", 
+    converte a escolha para letras minúsculas e verifica se é válida. 
+    Se a escolha for válida, ela é retornada; caso contrário, a função 
+    solicita ao usuário que escolha novamente até que uma escolha válida seja feita.
+
+    Retorna:
+        str: A escolha do jogador, que pode ser "pedra", "papel" ou "tesoura".
+    """
+
+    # escolha do usuario
+    esc_jogador: str = input('Escolha entre Pedra, Papel ou Tesoura: ')
+
+    # convertendo a escolha do usuário para letras minúsculas
     esc_jogador = esc_jogador.lower()
-    if esc_jogador == 'pedra':
-        return esc_jogador
-    elif esc_jogador == 'papel':
-        return esc_jogador
-    elif esc_jogador == 'tesoura':
+
+    if esc_jogador in ['pedra', 'papel', 'tesoura']:
         return esc_jogador
     else:
+        # caso o usuario escolha alguma opção que não exista
         print('Valor escolhido está errado...')
-        jogador()
+        return jogador()
 
 
-def maquina():
+def maquina() -> str:
+    """
+    Função para gerar uma escolha aleatória para a máquina em um jogo de Pedra, Papel e Tesoura.
+
+    A função utiliza o módulo random para escolher aleatoriamente entre "pedra", "papel" e "tesoura", 
+    retornando a escolha como uma string.
+
+    Retorna:
+        str: A escolha da máquina, que pode ser "pedra", "papel" ou "tesoura".
+    """
     esc_maquina = choice(['pedra', 'papel', 'tesoura'])
     return esc_maquina
 
 
-def jogo():
+def jogo() -> tuple:
+    """
+    Função para realizar um jogo de Pedra, Papel e Tesoura entre o jogador e a máquina.
+
+    A função realiza três rodadas do jogo, onde o jogador escolhe entre "pedra", "papel" e "tesoura",
+    e a máquina faz uma escolha aleatória. Os resultados de cada rodada são contabilizados para
+    determinar o número de vitórias de cada um.
+
+    Retorna:
+        tuple: Uma tupla contendo duas variáveis inteiras:
+            - jogador_vitoria: número de vitórias do jogador.
+            - maquina_vitoria: número de vitórias da máquina.
+    """
     jogador_vitoria = 0
     maquina_vitoria = 0
+
+    resultado = {
+        "tesoura": {
+            "tesoura": "empate",
+            "papel": "jogador venceu",
+            "pedra": "maquina venceu"
+        },
+        "papel": {
+            "tesoura": "maquina venceu",
+            "papel": "empate",
+            "pedra": "jogador venceu"
+        },
+        "pedra": {
+            "tesoura": "jogador venceu",
+            "papel": "maquina venceu",
+            "pedra": "empate"
+        }
+    }
     for x in range(3):
-        esc_jogador = jogador()
-        esc_maquina = maquina()
-        if esc_jogador == 'tesoura':
-            if esc_maquina == 'tesoura':
-                print('Deu empate')
+        # escolha do usuario
+        esc_jogador: str = jogador()
 
-            elif esc_maquina == 'papel':
-                print('Jogador venceu')
-                jogador_vitoria += 1
+        # escolha da maquina
+        esc_maquina: str = maquina()
 
-            elif esc_maquina == 'pedra':
-                print('Maquina venceu')
-                maquina_vitoria += 1
+        jogada = resultado[esc_jogador][esc_maquina]
 
-        elif esc_jogador == 'pedra':
-            if esc_maquina == 'tesoura':
-                print('Jogador venceu')
-                jogador_vitoria += 1
+        if jogada == "jogador venceu":
+            print("Usuario venceu a Rodada")
+            jogador_vitoria += 1
 
-            elif esc_maquina == 'papel':
-                print('Maquina venceu')
-                maquina_vitoria += 1
+        elif jogada == "maquina venceu":
+            print("Maquina venceu a rodada")
+            maquina_vitoria += 1
 
-            elif esc_maquina == 'pedra':
-                print('Deu empate')
+        else:
+            print("Empate")
 
-        elif esc_jogador == 'papel':
-            if esc_maquina == 'tesoura':
-                print('Maquina venceu')
-                maquina_vitoria += 1
-
-            elif esc_maquina == 'papel':
-                print('Deu empate')
-
-            elif esc_maquina == 'pedra':
-                print('Jogador venceu')
-                jogador_vitoria += 1
-
+    # retorna uma tupla com os resultados
     return jogador_vitoria, maquina_vitoria
 
 
 print('Vamos começar a jogar!')
 
-[jogador_funcao,maquina_funcao] = jogo()
+# passamos a variavel para receber o resultado do jogo
+[jogador_funcao, maquina_funcao] = jogo()
 
+# Loop principal do jogo
 while True:
+    # Adiciona os resultados das funções jogador_funcao e maquina_funcao às vitórias do jogador e da máquina
     jogador_vitoria += jogador_funcao
     maquina_vitoria += maquina_funcao
-    esc_jogador = input('Quer continue jogando? ')
 
+    # Pergunta ao jogador se ele deseja continuar jogando
+    esc_jogador = input('Quer continuar jogando? ')
+
+    # Verifica se o jogador deseja continuar jogando
     if esc_jogador in ['SIM', 'Sim', 's', 'sim', 'SiM', 'ss', 'Ss']:
-        [jogador_funcao,maquina_funcao] = jogo()
+        # Chama a função jogo e armazena os resultados em jogador_funcao e maquina_funcao
+        [jogador_funcao, maquina_funcao] = jogo()
 
+    # Verifica se o jogador deseja parar de jogar
     elif esc_jogador in ['NAO', 'Nao', 'n', 'nao', 'não', 'nn']:
+        # Imprime o resultado final das vitórias do jogador e da máquina
         print(
-            f'A maquina venceu {maquina_vitoria} vezes, O jogador venceu {jogador_vitoria} vezes!')
+            f'A máquina venceu {maquina_vitoria} vezes, o jogador venceu {jogador_vitoria} vezes!'
+        )
+        # Sai do loop
         break
+
+    # Caso o valor digitado não seja uma opção válida
     else:
-        print('Valor digitado não está na opção  de (s/n), vamos tentar novamente')
+        # Informa ao jogador que a opção digitada é inválida e solicita nova entrada
+        print(
+            'Valor digitado não está na opção de (s/n), vamos tentar novamente'
+        )
